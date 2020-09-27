72f89b142564b953d41c03725682fa14f070db93
Tóth Zsófia <zsofiath@gmail.com>
2020-09-27 19:45:10 +0200

    Creating Rotator converter with implemented IValueConverter interface

---
+++ src/AutomatedCar/Visualization/Rotator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using Avalonia.Data.Converters;
+
+namespace AutomatedCar.Visualization
+{
+    class Rotator : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
