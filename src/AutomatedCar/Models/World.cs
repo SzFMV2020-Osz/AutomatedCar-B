ecea83076dc248f344928254d1be2644ebace0a7
kovacsjozsef <kovacsjozsef@itdoki.hu>
2020-09-30 22:33:56 +0200

    Refactoring
    Refactoring

--- src/AutomatedCar/Models/World.cs
+++ src/AutomatedCar/Models/World.cs
@@ -7,7 +7,7 @@
     using Avalonia.Media;
     using ReactiveUI;
 
-    public class World : ReactiveObject, IWorld
+    public class World : ReactiveObject
     {
         // private static readonly System.Lazy<World> lazySingleton = new System.Lazy<World> (() => new World());
         // public static World Instance { get { return lazySingleton.Value; } }
@@ -33,16 +33,38 @@
             this.WorldObjects.Add(worldObject);
         }
 
-        public List<WorldObject> SearchInRange(List<Point> points)
+        /// <summary>
+        /// Returns an instance of a World object.
+        /// </summary>
+        /// <returns>Returns an instance of the World.</returns>
+        public static World GetInstance()
+        {
+            return new World();
+        }
+
+        /// <summary>
+        /// For sensors: Returns a list of worldobject that can be found in an area defined by the given list of points.
+        /// </summary>
+        /// <param name="searchArea">Area to be searched, defined by a list of points.</param>
+        /// <returns>Returns a list of worldobjects in a given area.</returns>
+        public List<WorldObject> SearchInRange(List<Point> searchArea)
         {
             throw new NotImplementedException();
         }
 
+        /// <summary>
+        /// For getting the controlledCars position, geometry etc.
+        /// </summary>
+        /// <returns>Returns the instance of the controlled car as an AutomatedCar.</returns>
         public AutomatedCar GetControlledCar()
         {
-            return Instance.ControlledCar;
+            throw new NotImplementedException();
         }
 
+        /// <summary>
+        /// For getting the NPC-s position, geometry etc.
+        /// </summary>
+        /// <returns>Returns a list of WorldObjects containing all the NPCs</returns>
         public List<WorldObject> GetNPCs()
         {
             throw new NotImplementedException();
