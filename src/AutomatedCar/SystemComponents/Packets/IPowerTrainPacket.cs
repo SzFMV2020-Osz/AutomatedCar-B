f47bdc16371adbc1a70799dd58f971675162f5d1
ddfdfg <szabo.rozso@gmail.com>
2020-10-04 13:05:07 +0200

    Create IPowerTrainPacket.cs
    Interface, amin a HMI Inputot ad a PowerTrainnek

---
+++ src/AutomatedCar/SystemComponents/Packets/IPowerTrainPacket.cs
@@ -0,0 +1,21 @@
+﻿namespace AutomatedCar.SystemComponents.Packets
+{
+    public enum GearShifterPosition
+    {
+        D,
+        N,
+        R,
+        P
+    }
+
+    public interface IPowerTrainPacket
+    {
+        public int GasPedal { get; set; }
+
+        public int BrakePedal { get; set; }
+
+        public int SteeringWheel { get; set; }
+
+        public GearShifterPosition GearShifterPosition { get; set; }
+    }
+}
