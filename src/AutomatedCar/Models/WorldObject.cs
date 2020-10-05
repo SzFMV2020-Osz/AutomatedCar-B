5f0e54a037e9b7787844096da491bb763375f64a
lindner.marton <lindner.marton@modit.hu>
2020-10-05 13:01:28 +0200

    fix

--- src/AutomatedCar/Models/WorldObject.cs
+++ src/AutomatedCar/Models/WorldObject.cs
@@ -8,7 +8,7 @@ namespace AutomatedCar.Models
     {
         private int _x;
         private int _y;
-        private double _angle = 90; // mocking
+        private double _angle;
         public int _rotationCenterPointX = 90; // = width/2
         public int _rotationCenterPointY = 120; // = height/2
 
@@ -30,6 +30,7 @@ namespace AutomatedCar.Models
         public double Angle
         {
             get => this._angle;
+            set => _angle = value;
         }
 
         public int RotationCenterPointX 
