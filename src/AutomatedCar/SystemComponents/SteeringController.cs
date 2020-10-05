3068076c9a67607f87216630f58d0ce4b47248eb
ddfdfg <szabo.rozso@gmail.com>
2020-10-06 01:25:21 +0200

    Update SteeringController.cs
    Elozoleg rossz szoget hasznaltam a kozeppont megallapitasahoz, ezert:
    -Az eltolasvektort kiszerveztem distanceFromCarCornerToCenter-be
    -hozza tartozo setter metodus hozzaadva, beleteve az UpdateSteeringWheel metodusba
    -NewCarPosition modositva: kozeppontrol visszavalt bal felso sarokpontra

--- src/AutomatedCar/SystemComponents/SteeringController.cs
+++ src/AutomatedCar/SystemComponents/SteeringController.cs
@@ -13,10 +13,11 @@
         private bool isInReverseGear;
         private float deltaTime = 1 / Game.TicksPerSecond;
         private Vector2 carCenterPoint;
+        private Vector2 distanceFromCarCornerToCenter;
 
         public Vector2 NewCarPosition
         {
-            get => Vector2.Divide(Vector2.Add(this.FrontWheel, this.BackWheel), 2);
+            get => ((this.FrontWheel + this.BackWheel) / 2) + (-1 * this.distanceFromCarCornerToCenter);
         }
 
         public double NewCarAngle
@@ -29,9 +30,7 @@
             get => this.carCenterPoint;
             set
             {
-                float distanceToCarCenterFromCorner = (float)Math.Sqrt(Math.Pow(World.Instance.ControlledCar.Width / 2, 2) + Math.Pow(World.Instance.ControlledCar.Height / 2, 2));
-                Vector2 displacementFromCarCorner = Vector2.Multiply(distanceToCarCenterFromCorner, new Vector2((float)Math.Cos(this.CarCurrentAngle - 45), (float)Math.Sin(this.CarCurrentAngle - 45)));
-                this.carCenterPoint = Vector2.Add(new Vector2(World.Instance.ControlledCar.X, World.Instance.ControlledCar.Y), displacementFromCarCorner);
+                this.carCenterPoint = Vector2.Add(new Vector2(World.Instance.ControlledCar.X, World.Instance.ControlledCar.Y), this.distanceFromCarCornerToCenter);
             }
         }
 
@@ -79,6 +78,7 @@
         {
             this.SteeringAngle = packet.SteeringWheel;
             this.isInReverseGear = packet.GearShifterPosition == GearShifterPosition.R;
+            this.SetDistanceFromCarCornerToCenter();
             this.SetWheelPositions();
             this.MoveWheelPositions();
         }
@@ -93,6 +93,13 @@
         {
             this.FrontWheel = Vector2.Add(this.FrontWheel, Vector2.Multiply(this.VelocityPixelPerTick, this.FrontWheelDirectionUnitVector));
             this.BackWheel = Vector2.Add(this.BackWheel, Vector2.Multiply(this.VelocityPixelPerTick, this.CarDirectionUnitVector));
+        }
+
+        private void SetDistanceFromCarCornerToCenter()
+        {
+            float distanceToCarCenterFromCorner = (float)Math.Sqrt(Math.Pow(World.Instance.ControlledCar.Width / 2, 2) + Math.Pow(World.Instance.ControlledCar.Height / 2, 2));
+            double angleDifferentialToCarAngle = 180 - Math.Asin((World.Instance.ControlledCar.Width / 2) / distanceToCarCenterFromCorner);
+            this.distanceFromCarCornerToCenter = Vector2.Multiply(distanceToCarCenterFromCorner, new Vector2((float)Math.Cos(this.CarCurrentAngle + angleDifferentialToCarAngle), (float)Math.Sin(this.CarCurrentAngle + angleDifferentialToCarAngle)));
         }
     }
 }
