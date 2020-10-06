ecb45de40349155961ee9fa1eb48778b94d1d5b3
ddfdfg <szabo.rozso@gmail.com>
2020-10-06 09:51:07 +0200

    Create PowerTrain.cs
    osztaly letrehozas, alap propertyk / fuggvenyek felirasa

---
+++ src/AutomatedCar/SystemComponents/PowerTrain.cs
@@ -0,0 +1,26 @@
+﻿namespace AutomatedCar.SystemComponents
+{
+    using System;
+    using AutomatedCar.SystemComponents.Packets;
+
+    public class PowerTrain : SystemComponent
+    {
+        public PowerTrain(VirtualFunctionBus functionBus)
+            : base(functionBus)
+        {
+            this.Engine = new EngineController();
+            this.Steering = new SteeringController();
+        }
+
+        private EngineController Engine { get; set; }
+
+        private SteeringController Steering { get; set; }
+
+        private IPowerTrainPacket Packet { get; set; }
+
+        public override void Process()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
