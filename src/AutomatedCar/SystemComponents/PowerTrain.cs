578f734c48852f47bfc4e710defbab9e8b10acbf
ddfdfg <szabo.rozso@gmail.com>
2020-10-06 09:49:30 +0200

    Update PowerTrain.cs
    d

--- src/AutomatedCar/SystemComponents/PowerTrain.cs
+++ src/AutomatedCar/SystemComponents/PowerTrain.cs
@@ -1,25 +1,25 @@
-﻿using AutomatedCar.SystemComponents.Packets;
-using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace AutomatedCar.SystemComponents
+﻿namespace AutomatedCar.SystemComponents
 {
+    using System;
+    using AutomatedCar.SystemComponents.Packets;
+
     public class PowerTrain : SystemComponent
     {
         public PowerTrain(VirtualFunctionBus functionBus)
             : base(functionBus)
         {
             this.Engine = new EngineController();
+            this.Steering = new SteeringController();
         }
 
         private EngineController Engine { get; set; }
+
+        private SteeringController Steering { get; set; }
 
         private IPowerTrainPacket Packet { get; set; }
 
         public override void Process()
         {
-            //world.instance.controlledcar -> itt elerni, IMovable implementacio utan SetPosition / MoveObject-tel mozgatni
             throw new NotImplementedException();
         }
     }
