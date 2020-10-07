1ea296dd0674dbd340b70d6f416bbba3960bad22
Tóth Zsófia <zsofiath@gmail.com>
2020-10-07 11:03:18 +0200

    test: (Visualisation) :white_check_mark: adding a blank test

---
+++ test/AutomatedCarTests/Visualisation/AlignItemsToScreenTest.cs
@@ -0,0 +1,21 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace VisualisationTests
+{
+    [TestClass]
+    public class AlignItemsToScreenTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            // Arrange
+            
+
+            // Act
+            
+
+            // Assert
+            Assert.AreEqual(true, true);
+        }
+    }
+}
