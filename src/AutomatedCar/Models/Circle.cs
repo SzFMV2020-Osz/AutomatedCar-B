7b2338e7f979814535e20216e1030338cf638630
pintergreg <pintergreg@gmail.com>
2020-10-08 16:04:41 +0200

    Bind image

--- src/AutomatedCar/Models/Circle.cs
+++ src/AutomatedCar/Models/Circle.cs
@@ -1,21 +1,25 @@
 namespace AutomatedCar.Models
 {
     using System;
+    using System.Reflection;
+    using Avalonia.Media.Imaging;
 
     /// <summary>This is a dummy object for demonstrating the codebase.</summary>
-    public class Circle : WorldObject
+public class Circle : WorldObject
+{
+    private Bitmap image;
+    public Circle(int x, int y, string filename, int radius)
+        : base(x, y, filename)
     {
-        public Circle(int x, int y, string filename, int radius)
-            : base(x, y, filename)
-        {
-            this.Radius = radius;
-        }
-
-        public int Radius { get; set; }
-
-        public double CalculateArea()
-        {
-            return Math.PI * this.Radius * this.Radius;
-        }
+        this.Radius = radius;
+        image = new Bitmap(Assembly.GetExecutingAssembly().GetManifestResourceStream($"AutomatedCar.Assets.WorldObjects.{filename}"));
     }
+
+    public int Radius { get; set; }
+    public Bitmap Image { get => this.image; }
+    public double CalculateArea()
+    {
+        return Math.PI * this.Radius * this.Radius;
+    }
+}
 }
