873a0d15d2cd6854a7ff00f4f59cff5bec480490
dumbi <davidmeszaros996@gmail.com>
2020-10-13 22:48:17 +0200

    Adde missing files

---
+++ src/AutomatedCar/Logic/JsonReferences.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AutomatedCar.Logic
+{
+    class JsonReferences
+    {
+        public int ReferenceOffsetX;
+        public int ReferenceOffsetY;
+    }
+}
---
+++ src/AutomatedCar/Logic/JsonWorldObject.cs
@@ -0,0 +1,16 @@
+﻿
+using Avalonia;
+using System.Collections.Generic;
+
+namespace AutomatedCar.Logic
+{
+    public class JsonWorldObject
+    {
+        public int X;
+        public int Y;
+        public string Filename;
+        public int Width;
+        public int Height;
+        public Matrix Rotmatrix;
+    }
+}
