f01edc16eaa464a8dd67aa00628ada34cee83641
Csonka Bálint <csonkabalintka@gmail.com>
2020-10-17 12:28:43 +0200

    Modify Powertrain to use HMIpacket

--- src/AutomatedCar/SystemComponents/PowerTrain.cs
+++ src/AutomatedCar/SystemComponents/PowerTrain.cs
@@ -1,4 +1,4 @@
-﻿namespace AutomatedCar.SystemComponents
+namespace AutomatedCar.SystemComponents
 {
     using System;
     using AutomatedCar.Models;
@@ -11,33 +11,39 @@
         {
             this.Engine = new EngineController();
             this.Steering = new TestSteering();
-            this.Packet = this.virtualFunctionBus.PowerTrainPacket;
+            this.Packet = this.virtualFunctionBus.HMIPacket;
         }
 
         private EngineController Engine { get; set; }
 
         private ISteeringController Steering { get; set; }
 
-        private IPowerTrainPacket Packet { get; set; }
+        private IReadOnlyHMIPacket Packet { get; set; }
 
         public override void Process()
         {
             this.Engine.UpdateEngineProperties(this.Packet);
             this.Steering.UpdateSteeringProperties(this.Packet);
+            //GEAR DOES NOT CHANGE
+            this.Engine.UpdateEngineProperties(testHMIpacket);
+            this.Steering.UpdateSteeringProperties(testHMIpacket);
             this.UpdateCarPosition();
-            this.UpdateHMIPacket();
+            this.UpdatePowerTrainPacket();
         }
 
         private void UpdateCarPosition()
         {
             World.Instance.ControlledCar.Move(this.Steering.NewCarPosition);
+            //World.Instance.ControlledCar.Move(new System.Numerics.Vector2(50,50));
+            //Console.Write(this.Steering.NewCarPosition.X);
+            //Console.WriteLine(this.Steering.NewCarPosition.Y);
             World.Instance.ControlledCar.Angle = this.Steering.NewCarAngle;
             World.Instance.ControlledCar.Speed = (int)this.Engine.VelocityPixelsPerSecond;
         }
 
-        private void UpdateHMIPacket()
+        private void UpdatePowerTrainPacket()
         {
-            this.virtualFunctionBus.HMIPacket.UpdateHMIPacket(this.Engine.VelocityPixelsPerSecond, this.Engine.RPM, this.Engine.GearShifter.Position, this.Engine.GearShifter.CurrentDriveGear.Label);
+           //TODO change To PowerTrainPacket this.virtualFunctionBus.HMIPacket.UpdateHMIPacket(this.Engine.VelocityPixelsPerSecond, this.Engine.RPM, this.Engine.GearShifter.Position, this.Engine.GearShifter.CurrentDriveGear.Label);
         }
     }
 }
