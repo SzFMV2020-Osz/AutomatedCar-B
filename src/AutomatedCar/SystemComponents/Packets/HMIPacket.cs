b2af0c7e951b12ea7adcfbe283ec197e2fbf37c3
ddfdfg <szabo.rozso@gmail.com>
2020-10-17 10:54:42 +0200

    Update HMIPacket.cs
    HMI Packet conflict feloldas (HMI "atvette" a class nevet, ezert ujradefinialtuk)

--- src/AutomatedCar/SystemComponents/Packets/HMIPacket.cs
+++ src/AutomatedCar/SystemComponents/Packets/HMIPacket.cs
@@ -1,52 +1,3 @@
-<<<<<<< HEAD
-﻿using ReactiveUI;
-using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace AutomatedCar.SystemComponents.Packets
-{
-    class HMIPacket : ReactiveObject, IHMIPacket
-    {
-        private double velocityPixelsPerSecond;
-        private int rpm;
-        private GearShifterPosition gearShifterPosition;
-        private DriveGear driveGear;
-
-        public void UpdateHMIPacket(double speed, int rpm, GearShifterPosition position, DriveGear driveGear)
-        {
-            this.VelocityPixelsPerSecond = speed;
-            this.RPM = rpm;
-            this.GearShifterPostion = position;
-            this.DriveGear = driveGear;
-        }
-
-        public double VelocityPixelsPerSecond
-        {
-            get => this.velocityPixelsPerSecond;
-            private set => this.RaiseAndSetIfChanged(ref this.velocityPixelsPerSecond, value);
-        }
-
-        public int RPM
-        {
-            get => this.rpm;
-            private set => this.RaiseAndSetIfChanged(ref this.rpm, value);
-        }
-
-        public GearShifterPosition GearShifterPostion
-        {
-            get => this.gearShifterPosition;
-            private set => this.RaiseAndSetIfChanged(ref this.gearShifterPosition, value);
-        }
-
-        public DriveGear DriveGear
-        {
-            get => this.driveGear;
-            private set => this.RaiseAndSetIfChanged(ref this.driveGear, value);
-        }
-    }
-}
-=======
 using Avalonia.OpenGL;
 using System;
 namespace AutomatedCar.SystemComponents.Packets
@@ -339,4 +290,3 @@ namespace AutomatedCar.SystemComponents.Packets
         }
     }
 }
->>>>>>> master
