c30c04ae85c0f179e2c14519c7fb455a276b2e11
ddfdfg <szabo.rozso@gmail.com>
2020-10-17 10:52:26 +0200

    Update Game.cs
    Mockolt kontrollok kiszedese (powertrainre felkeszites)

--- src/AutomatedCar/Game.cs
+++ src/AutomatedCar/Game.cs
@@ -18,46 +18,6 @@ namespace AutomatedCar
         protected override void Tick()
         {
             world.ControlledCar.InputHandler();
-
-            int speed = 10;
-
-            if (Keyboard.IsKeyDown(Key.Space))
-            {
-                speed = 300;
-            }
-
-            world.ControlledCar.InputHandler();
-
-            if (Keyboard.IsKeyDown(Key.Up))
-            {
-                this.world.ControlledCar.MoveY(-speed);
-            }
-
-            if (Keyboard.IsKeyDown(Key.Down))
-            {
-                this.world.ControlledCar.MoveY(speed);
-            }
-
-            if (Keyboard.IsKeyDown(Key.Left))
-            {
-                this.world.ControlledCar.MoveX(-speed);
-            }
-
-            if (Keyboard.IsKeyDown(Key.Right))
-            {
-                this.world.ControlledCar.MoveX(speed);
-            }
-
-            if (Keyboard.IsKeyDown(Key.B))
-            {
-                this.world.ControlledCar.Angle += 5;
-            }
-
-            if (Keyboard.IsKeyDown(Key.N))
-            {
-                this.world.ControlledCar.Angle -= 5;
-            }
-
             World.Instance.Tick();
         }
     }
