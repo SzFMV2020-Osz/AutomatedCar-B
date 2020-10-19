54374857f2e38ea10f8f711c834426b4ad25073c
Travnik Gergely <travnik.gergely@gmail.com>
2020-10-19 18:43:39 +0200

    Corrections after pull from master, powertrain throws null reference exception

--- src/AutomatedCar/Views/Converters/IntegerToAngleRpmConverter.cs
+++ src/AutomatedCar/Views/Converters/IntegerToAngleRpmConverter.cs
@@ -8,18 +8,18 @@ namespace AutomatedCar.Views.Converters
     using Avalonia.Data.Converters;
     using Avalonia.Media;
 
-    public class DoubleToAngleRpmConverter : IValueConverter
+    public class IntegerToAngleRpmConverter : IValueConverter
     {
         private static readonly double MaxAngle = 260;
         private static readonly double MaxRpm = 100;
         private static readonly double Shifting = 40;
 
-        public DoubleToAngleRpmConverter()
+        public IntegerToAngleRpmConverter()
         {
         }
 
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
-            => ((double)value * MaxAngle / MaxRpm) - Shifting;
+            => ((int)value * MaxAngle / MaxRpm) - Shifting;
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         {
