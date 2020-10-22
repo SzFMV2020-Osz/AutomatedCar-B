47a3348b6ac39e4c374b931e63f3a9681a94e1d9
Roland Darvas <roland.darvas@gmail.com>
2020-10-22 18:47:26 +0200

    GetWorldObjectsInsideTriangle new implementation, WorldTest.cs alpha version

---
+++ test/AutomatedCarTests/Models/WorldTest.cs
@@ -0,0 +1,25 @@
+﻿namespace Tests.Models
+{
+    using System;
+    using AutomatedCar.Logic;
+    using AutomatedCar.Models;
+    using Xunit;
+
+    public class WorldTest
+    {
+        private World word;
+
+        public WorldTest()
+        {
+            JsonParser parser = new JsonParser();
+            parser.populateWorldObjects(World.Instance, $"AutomatedCarTests.Assets.test_world.json");
+            this.word = World.Instance;
+        }
+
+        [Fact]
+        public void TestTriangleDetection()
+        {
+            Console.WriteLine("ASDASD");
+        }
+    }
+}
