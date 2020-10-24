e52d42ca1d65a4a2aac4773ed2fb10279f7f6543
Roland Darvas <roland.darvas@gmail.com>
2020-10-24 14:46:20 +0200

    Test helper methods created.

--- test/AutomatedCarTests/Models/WorldTest.cs
+++ test/AutomatedCarTests/Models/WorldTest.cs
@@ -1,25 +1,84 @@
 ﻿namespace Tests.Models
 {
     using System;
-    using AutomatedCar.Logic;
+    using System.Collections.Generic;
     using AutomatedCar.Models;
+    using Avalonia;
     using Xunit;
 
     public class WorldTest
     {
-        private World word;
+        private World world;
+
+        private Matrix testRotMatrix;
+
+        private Random random;
 
         public WorldTest()
         {
-            JsonParser parser = new JsonParser();
-            parser.populateWorldObjects(World.Instance, $"AutomatedCarTests.Assets.test_world.json");
-            this.word = World.Instance;
+            this.random = new Random();
+            this.testRotMatrix = new Matrix(1, 0, 0, 1, 0, 0);
+            this.world = World.Instance;
+            InitWorld();
         }
 
         [Fact]
         public void TestTriangleDetection()
         {
             Console.WriteLine("ASDASD");
+        }
+
+        private WorldObject CreateRoad()
+        {
+            return new Road(10, 10, null, 10, 10, 0, 0,
+                testRotMatrix, CreateRandomPolyPoints());
+        }
+
+        private WorldObject CreateSign()
+        {
+            return new Sign(10, 5, null, 5, 5, 0, 0,
+                testRotMatrix, CreateRandomPolyPoints());
+        }
+
+        private WorldObject CreateTree()
+        {
+            return new Tree(20, 10, null, 5, 5, 0, 0,
+                testRotMatrix, CreateRandomPolyPoints());
+        }
+
+        private WorldObject CreateParkingSpot()
+        {
+            return new ParallelParking(30, 30, null, 20, 10, 0, 0,
+                testRotMatrix, CreateRandomPolyPoints());
+        }
+
+        private WorldObject CreateGarage()
+        {
+            return new Garage(50, 20, null, 20, 20, 0, 0,
+                testRotMatrix, CreateRandomPolyPoints());
+        }
+
+        private List<List<Point>> CreateRandomPolyPoints()
+        {
+            List<List<Point>> polyPoints = new List<List<Point>>();
+
+            Point p1 = new Point(this.random.Next(1, 101), this.random.Next(1, 101));
+            Point p2 = new Point(this.random.Next(1, 101), this.random.Next(1, 101));
+            List<Point> points = new List<Point>();
+            points.Add(p1);
+            points.Add(p2);
+            
+            polyPoints.Add(points);
+            return polyPoints;
+        }
+        
+        private void InitWorld()
+        {
+            this.world.AddObject(CreateRoad());
+            this.world.AddObject(CreateSign());
+            this.world.AddObject(CreateTree());
+            this.world.AddObject(CreateParkingSpot());
+            this.world.AddObject(CreateGarage());
         }
     }
 }
