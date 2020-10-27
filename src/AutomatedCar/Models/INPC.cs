7f3d12f996a76ceda3dbc862be67d053565a7700
Csonka Bálint <csonkabalintka@gmail.com>
2020-10-27 18:02:29 +0100

    Label INPC interface as a descendant of IMoveable interface

--- src/AutomatedCar/Models/INPC.cs
+++ src/AutomatedCar/Models/INPC.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace AutomatedCar.Models
 {
-    public interface INPC
+    public interface INPC : IMoveable
     {
         public int Rotation { get; set; }
         public int Speed { get; set; }
