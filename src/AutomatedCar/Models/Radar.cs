6a3615bfe10c05c5701531decd7f1895932addc0
Sari <Sari@DESKTOP-BGJ1DJH>
2020-10-30 23:30:23 +0100

    B4_OsztalyokLetrehozasa_#145

---
+++ src/AutomatedCar/Models/Radar.cs
@@ -0,0 +1,73 @@
+using Avalonia;
+using NetTopologySuite.GeometriesGraph;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AutomatedCar.Models
+{
+    public class Radar
+    {
+        List<NoticedObject> noticedObjects;
+        Position carPreviousPosition;
+        Point[3] points;
+
+        public List<NoticedObject> NoticedObjects { get => noticedObjects; set => noticedObjects = value; }
+        public Position CarPreviousPosition { get => carPreviousPosition; set => carPreviousPosition = value; }
+        public Point[] Points { get => points; set => points = value; }
+
+        public List<NoticedObject> filterCollidables(List<NoticedObject> paramNoticedObjects)
+        {
+            return null;
+        }
+
+        public void computeVector(NoticedObject paramNoticedObject)
+        {
+
+        }
+
+        public void setAllSeen()
+        {
+
+        }
+
+        public bool isInNoticedObjects(WorldObject paramWorldObject)
+        {
+            return true;
+        }
+        public void setHighlighted(WorldObject paramWorldObject)
+        {
+            
+        }
+
+        public void updatePreviewXY()
+        {
+
+        }
+        public void deleteLeftObjects()
+        {
+
+        }
+
+        public NoticedObject newObjectIsDetected()
+        {
+            return null;
+        }
+
+        public Point[] computeTriangleInWorld()
+        {
+
+        }
+
+        public void updateBus()
+        {
+
+        }
+
+        public List<WorldObject> getDangeriousWorldObjects()
+        {
+            return null;
+        }
+
+    }
+}
