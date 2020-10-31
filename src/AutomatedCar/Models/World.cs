33003cd868c8c16caa69e22f53ab30acaf9792ed
BognarSzGergo <gergo9919@gmail.com>
2020-10-31 21:50:02 +0100

    IsColisonWhitWorldObject is done

--- src/AutomatedCar/Models/World.cs
+++ src/AutomatedCar/Models/World.cs
@@ -26,6 +26,12 @@ namespace AutomatedCar.Models
 
         public OnTickHangler OnTick;
 
+        public delegate void CollisonEventHandler(WorldObject worldObject);
+
+        public CollisonEventHandler OnCollideWithLandmark;
+
+        public CollisonEventHandler OnCollideWithNPC;
+
         public bool DebugOn { get => this.debugOn; }
 
         public static World Instance { get; } = new World() { VisibleWidth = 960, VisibleHeight = 720};
@@ -120,19 +126,29 @@ namespace AutomatedCar.Models
             return objectsInside;
         }
 
-        //public WorldObject IsColisonWhitWorldObject() {
 
-        //    foreach (WorldObject item in this.WorldObjects)
-        //    {
-        //        foreach (LineString polygon in item.NetPolygons)
-        //        {
-        //            if (_controlledCar.NetPolygons.Intersects())
-        //            {
-        //                objectsInside.Add(item);
-        //                break;
-        //            }
-        //        }
-        //    }
-        //}
+        public void IsColisonWhitWorldObject()
+        {
+            foreach (WorldObject item in this.WorldObjects.Where(x => x.IsColliding))
+            {
+                foreach (NetTopologySuite.Geometries.LineString worldObjectpolygon in item.NetPolygons)
+                {
+                    foreach (NetTopologySuite.Geometries.LineString carPolygon in this._controlledCar.NetPolygons)
+                    {
+                        if (worldObjectpolygon.Intersects(carPolygon))
+                        {
+                            if (item is IMoveable)
+                            {
+                                OnCollideWithNPC?.Invoke(item);
+                            }
+                            else
+                            {
+                                OnCollideWithLandmark?.Invoke(item);
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
 }
