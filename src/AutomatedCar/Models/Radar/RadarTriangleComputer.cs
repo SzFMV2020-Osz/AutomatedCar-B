60128bf22d8c706c8258162a9440a3abdd84ff3e
Tóth Zsófia <zsofiath@gmail.com>
2020-10-31 17:22:18 +0100

    refactor

--- src/AutomatedCar/Models/Radar/RadarTriangleComputer.cs
+++ src/AutomatedCar/Models/Radar/RadarTriangleComputer.cs
@@ -9,23 +9,27 @@ namespace AutomatedCar.Models.RadarUtil
     public class RadarTriangleComputer
     { 
         public int offset = 0;
-        public double distance = 0;
+        public int distance = 0;
         public Point[] computeTriangleInWorld()
         {
             Point[] points = new Point[3];
 
 
-            double atfogo = distance/Math.Cos((Math.PI / 180) * 30);
-            double a2 = Math.Pow(atfogo, 2)-Math.Pow(distance, 2);
-
-            double eredmeny = Math.Round(Math.Sqrt(a2));
+            int xWidth = getXfromAngleAndDistance(this.distance, 30);
 
 
             points[0] = new Point(0,this.offset);
-            points[1] = new Point(eredmeny,this.offset + this.distance);
-            points[2] = new Point(-eredmeny,this.offset + this.distance);
+            points[1] = new Point(xWidth,this.offset + this.distance);
+            points[2] = new Point(-xWidth,this.offset + this.distance);
 
             return points;
+        }
+
+        private int getXfromAngleAndDistance(int distance, int angle) {
+            double hypotenuse = distance/Math.Cos((Math.PI / 180) * angle);
+            double powered = Math.Pow(hypotenuse, 2)-Math.Pow(distance, 2);
+
+            return (int)Math.Round(Math.Sqrt(powered));
         }
     }
 }
