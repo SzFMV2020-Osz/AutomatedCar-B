2b244a3cbb79bed290522c22f896286e78260bd9
Tóth Zsófia <zsofiath@gmail.com>
2020-10-31 12:41:06 +0100

    adding dummy test

---
+++ test/AutomatedCarTests/SystemComponents/Packets/RadarTest.cs
@@ -0,0 +1,18 @@
+using AutomatedCar.SystemComponents;
+using AutomatedCar.SystemComponents.Packets;
+using Avalonia.Layout;
+using Microsoft.VisualStudio.TestPlatform.CommunicationUtilities.ObjectModel;
+using System.ComponentModel;
+using Xunit;
+
+namespace Tests.SystemComponents.Packets
+{
+    public class RadarTest
+    {
+        [Fact]
+        public void Radar()
+        {
+            Assert.Equal(true,true);
+        }
+    }
+}
