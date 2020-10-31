7a5d3b7d976c86dac94a4691b64b1090006cb055
Tóth Zsófia <zsofiath@gmail.com>
2020-10-31 21:33:31 +0100

    format code

--- src/AutomatedCar/Models/Radar/RadarTriangleComputer.cs
+++ src/AutomatedCar/Models/Radar/RadarTriangleComputer.cs
@@ -16,24 +16,31 @@ namespace AutomatedCar.Models.RadarUtil
         {
             Point[] points = new Point[3];
 
-            var ca = Math.Round(Math.Cos((Math.PI / 180) * rotate));
-            var sa = Math.Round(Math.Sin((Math.PI / 180) * rotate));
+            var ca = cos(rotate);
+            var sa = sin(rotate);
 
             int y = getXfromAngleAndDistance((int)(this.distance*ca), this.angle);
 
-            points[0] = new Point(this.offset*ca, this.offset*sa);
-            points[1] = new Point(this.offset*ca + distance*ca, this.offset*sa +y);
-            points[2] = new Point(this.offset*ca + distance*ca, this.offset*sa -y);
-
+            points[0] = new Point(this.offset * ca, this.offset * sa);
+            points[1] = new Point(this.offset * ca + distance * ca, this.offset * sa + y);
+            points[2] = new Point(this.offset * ca + distance * ca, this.offset * sa - y);
 
             return points;
         }
 
         private int getXfromAngleAndDistance(int distance, int angle) {
-            double hypotenuse = distance/Math.Cos((Math.PI / 180) * angle);
-            double powered = Math.Pow(hypotenuse, 2)-Math.Pow(distance, 2);
+            double hypotenuse = distance / Math.Cos((Math.PI / 180) * angle);
+            double powered = Math.Pow(hypotenuse, 2) - Math.Pow(distance, 2);
 
             return (int)Math.Round(Math.Sqrt(powered));
+        }
+
+        private double cos(int rotate){
+            return Math.Round(Math.Cos((Math.PI / 180) * rotate));
+        }
+
+        private double sin(int rotate){
+            return Math.Round(Math.Sin((Math.PI / 180) * rotate));
         }
     }
 }
