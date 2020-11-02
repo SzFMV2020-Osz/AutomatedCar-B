43d5beaf46372e56351ee592ae4b68d56bbda2d0
Tóth Zsófia <zsofiath@gmail.com>
2020-11-02 11:20:22 +0100

    init a test

---
+++ test/AutomatedCarTests/SystemComponents/Packets/Radar.filterCollidables_test.cs
@@ -0,0 +1,18 @@
+using AutomatedCar.SystemComponents;
+using AutomatedCar.SystemComponents.Packets;
+using Avalonia.Layout;
+using Microsoft.VisualStudio.TestPlatform.CommunicationUtilities.ObjectModel;
+using System.ComponentModel;
+using Xunit;
+
+namespace Tests.SystemComponents.Packets
+{
+    public class FilterCollidables
+    {
+        [Fact]
+        public void GaspedalExsits()
+        {
+            Assert.Equal("expected", "expected");
+        }
+    }
+}
