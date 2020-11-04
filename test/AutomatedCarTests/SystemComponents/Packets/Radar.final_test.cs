54ba91724e8533d3870b0aa35a4511277ba3b97b
Tóth Zsófia <zsofiath@gmail.com>
2020-11-04 20:06:34 +0100

    create test

---
+++ test/AutomatedCarTests/SystemComponents/Packets/Radar.final_test.cs
@@ -0,0 +1,25 @@
+using AutomatedCar.SystemComponents;
+using AutomatedCar.Models;
+using AutomatedCar.SystemComponents.Packets;
+using Avalonia.Layout;
+using Microsoft.VisualStudio.TestPlatform.CommunicationUtilities.ObjectModel;
+using System.ComponentModel;
+using System.Collections.Generic;
+using Xunit;
+using System.Linq;
+
+namespace Tests.SystemComponents.Packets
+{
+    public class FilterCollidables
+    {
+        [Fact]
+        public void FinalTest1()
+        {
+            
+
+            Assert.Equal(true, true);
+        }
+
+
+    }
+}
