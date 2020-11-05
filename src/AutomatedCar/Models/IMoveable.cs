de0a1fe87c5a2241b22065235c6a9f9b77b7b70e
Travnik Gergely <travnik.gergely@gmail.com>
2020-11-05 08:46:31 +0100

    y

--- src/AutomatedCar/Models/IMoveable.cs
+++ src/AutomatedCar/Models/IMoveable.cs
@@ -14,10 +14,5 @@ namespace AutomatedCar.Models
         public void SetNextPosition(int x, int y);
 
         public void Move(Vector2 with);
-      
-        public void MoveX(int x);
-
-        public void MoveY(int y);
-
     }
 }
