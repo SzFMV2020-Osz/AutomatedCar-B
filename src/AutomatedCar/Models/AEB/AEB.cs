6cf752361a363ccff7255401daab45e4ec92dfcf
zsofi <zsofiath@gmail.com>
2020-11-19 09:32:58 +0100

    AEB: :sparkles: adding AEB test

--- src/AutomatedCar/Models/AEB/AEB.cs
+++ src/AutomatedCar/Models/AEB/AEB.cs
@@ -1,7 +1,12 @@
+using AutomatedCar.SystemComponents;
+
 namespace AutomatedCar.Models
 {
     public class AEB
     {
-        
+        public AEB()
+        {
+            
+        }
     }
 }
---
+++ test/AutomatedCarTests/SystemComponents/Packets/AEBTest.cs
@@ -0,0 +1,23 @@
+namespace Tests.SystemComponents.Packets
+{
+    using System;
+    using System.Collections.Generic;
+    using AutomatedCar.Models;
+    using Avalonia;
+    using Xunit;
+    
+    public class AEBTest {
+
+        [Fact]
+        public void TestName()
+        {
+            //Given
+            AEB aeb = new AEB();
+            
+            //When
+            
+            //Then
+            Assert.Equal(true, true);
+        }
+    }
+}
