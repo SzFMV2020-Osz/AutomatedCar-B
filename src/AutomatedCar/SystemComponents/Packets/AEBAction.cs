4da7377d82e6d75a8ea9eedc7ad6b1f7a11af4c8
zsofi <zsofiath@gmail.com>
2020-12-01 11:28:36 +0100

    code style

--- src/AutomatedCar/SystemComponents/Packets/AEBAction.cs
+++ src/AutomatedCar/SystemComponents/Packets/AEBAction.cs
@@ -3,10 +3,8 @@ namespace AutomatedCar.SystemComponents.Packets
     using ReactiveUI;
     public class EABAction : ReactiveObject, IPowerTrainAction
     {
-        public bool Active {get; set;}
-
+        public bool Active { get; set; }
         private double breakpedal;
         public double Breakpedal { get => this.breakpedal; set => this.RaiseAndSetIfChanged(ref this.breakpedal, value); }
-
     }
 }
