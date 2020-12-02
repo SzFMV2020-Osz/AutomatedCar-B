e6c4b2c4ae63938cac5a40e73973cbd835c9e1cc
Aaron Rancsik <20153302+aaronrancsik@users.noreply.github.com>
2020-12-02 20:15:59 +0100

    make initial adaptive tempomat

--- src/AutomatedCar/SystemComponents/AccController.cs
+++ src/AutomatedCar/SystemComponents/AccController.cs
@@ -17,33 +17,75 @@ namespace AutomatedCar.SystemComponents
         {
             var hmiPacket = (HMIPacket)virtualFunctionBus.HMIPacket;
             var powerTrainPacket = (PowerTrainPacket)virtualFunctionBus.PowerTrainPacket;
-            var speed = kmh_to_pxs(hmiPacket.AccSpeed);
+            var speed = hmiPacket.AccSpeed;
 
             if (hmiPacket.Acc)
             {
+                hmiPacket.Gaspedal = 0;
                 if (hmiPacket.Sign != null && hmiPacket.Sign != "")
                 {
                     var lastSeenSign = int.Parse(hmiPacket.Sign);
-                    if (kmh_to_pxs(hmiPacket.AccSpeed) > kmh_to_pxs(lastSeenSign))
+                    if (speed > lastSeenSign)
                     {
-                        speed = kmh_to_pxs(lastSeenSign);
+                        speed = lastSeenSign;
                     }
                 }
 
-                if (powerTrainPacket.Velocity < speed)
+                if (powerTrainPacket.Velocity < Kmh2pxs(speed))
                 {
-                    hmiPacket.Gaspedal = 40;
+                    hmiPacket.Gaspedal = 100;
                 }
-                else if (powerTrainPacket.Velocity > speed)
+
+                var car = World.Instance.ControlledCar;
+                var radar = car.Radar;
+                var wos = radar.WorldObjects;
+                foreach (var wo in wos)
                 {
-                    hmiPacket.Gaspedal = 0;
+                    if (wo is NpcCar)
+                    {
+                        var wom = (wo.Angle + 90) % 360;
+                        var cam = car.Angle % 360;
+                        var dif = Math.Abs(wom - cam) % 360;
+
+                        if (dif > 180)
+                        {
+                            dif = 360 - dif;
+                        }
+
+                        if (dif > 60)
+                        {
+                            var cv = new Vector2(car.X, car.Y);
+                            var nv = new Vector2((wo as NpcCar).X, (wo as NpcCar).Y);
+                            var dis = Vector2.Distance(cv, nv);
+
+                            if (dis < 50 * Kmh2ms(Pxs2kmh(powerTrainPacket.Velocity)) * hmiPacket.AccDistance)
+                            {
+                                hmiPacket.Gaspedal = 0;
+                            }
+                        }
+                    }
                 }
             }
         }
 
-        private static int kmh_to_pxs(int AccSpeed)
+        private static double Map(double from, double oldMax, double oldMin, double newMax, double newMin)
         {
-            return AccSpeed * (50 * 1000) / (60 * 60);
+            return ((from - oldMin) / (oldMax - oldMin)) * (newMax - newMin) + newMin;
+        }
+
+        private static double Kmh2pxs(double kmh)
+        {
+            return kmh * (double)(50 * 1000) / (double)(60 * 60);
+        }
+
+        private static double Pxs2kmh(double pxs)
+        {
+            return pxs * (double)(60 * 60) / (double)(50 * 1000);
+        }
+
+        private static double Kmh2ms(double kmh)
+        {
+            return kmh / 3.6;
         }
     }
 }
